910a5adf15c186b457edb68874ff6ef28d3c78f2
GeorgiyEloev <geloev.exceedteam@gmail.com>
2021-12-17 12:19:11 +0300

    finish localStorage

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ window.onload = () => {
   inputShop.addEventListener("change", updateShop);
   inputMoney.addEventListener("change", updateMoney);
 
-	renderShop();	
+  renderShop();
 };
 
 const updateShop = (event) => {
@@ -52,11 +52,151 @@ const renderShop = () => {
   }
 
   arrayShoping.map((item, index) => {
-		const {shop, date, money} = item;
+    const { shop, date, money } = item;
 
-		const listGroupItem = document.createElement("div");
-		const nameShop = document.createElement("p");
-		const listGroupItemDateImg = document.createElement("div");
-		const 
-	});
+    const objItem = createItem(index, shop, date, money);
+
+    const listGroupItem = document.createElement("div");
+    listGroupItem.className = "list-group-item";
+    listGroupItem.id = `item-${index}`;
+
+    listGroupItem.appendChild(objItem.name);
+    listGroupItem.appendChild(objItem.date);
+    listGroup.appendChild(listGroupItem);
+  });
+
+  updateResult();
+};
+
+const delItem = (index) => {
+  arrayShoping.splice(index, 1);
+  localStorage.setItem("shop", JSON.stringify(arrayShoping));
+  renderShop();
+};
+
+const editItem = (index) => {
+  const listGroupItem = document.getElementById(`item-${index}`);
+  listGroupItem.className = "edit-item";
+
+  while (listGroupItem.firstChild) {
+    listGroupItem.removeChild(listGroupItem.firstChild);
+  }
+
+  const { shop, date, money } = arrayShoping[index];
+  let newDate = "";
+  const subDate = date.split(".");
+  newDate = subDate[2] + "-" + subDate[1] + "-" + subDate[0];
+
+  const nameShop = document.createElement("input");
+  const textDate = document.createElement("input");
+  const textMoney = document.createElement("input");
+  const listGroupItemImg = document.createElement("div");
+  const okImg = document.createElement("img");
+  const delImg = document.createElement("img");
+
+  nameShop.type = "text";
+  nameShop.id = `text-${index}`;
+  nameShop.value = shop;
+  textDate.type = "date";
+  textDate.id = `date-${index}`;
+  textDate.value = newDate;
+  textMoney.type = "number";
+  textMoney.id = `money-${index}`;
+  textMoney.value = money;
+  listGroupItemImg.className = "list-group-item-img";
+  okImg.src = "https://img.icons8.com/ios/50/000000/ok--v1.png";
+  delImg.src = "https://img.icons8.com/ios/100/000000/cancel.png";
+
+  okImg.onclick = () => changeItem(index);
+  delImg.onclick = () => closeChange(index, shop, date, money);
+
+  listGroupItemImg.appendChild(okImg);
+  listGroupItemImg.appendChild(delImg);
+  listGroupItem.appendChild(nameShop);
+  listGroupItem.appendChild(textDate);
+  listGroupItem.appendChild(textMoney);
+  listGroupItem.appendChild(listGroupItemImg);
+};
+
+const updateResult = () => {
+  const resultMoney = document.getElementById("result-money");
+  let sum = 0;
+
+  arrayShoping.map((item) => {
+    sum = sum + Number(item.money);
+  });
+
+  resultMoney.innerText = `Итог: ${sum} р.`;
+};
+
+const createItem = (index, shop, date, money) => {
+  const nameShop = document.createElement("p");
+  const listGroupItemDateImg = document.createElement("div");
+  const listGroupItemDateMoney = document.createElement("div");
+  const textDate = document.createElement("p");
+  const textMoney = document.createElement("p");
+  const listGroupItemImg = document.createElement("div");
+  const editImg = document.createElement("img");
+  const delImg = document.createElement("img");
+
+  nameShop.innerText = `${index + 1}) Магазин \"${shop}\"`;
+  listGroupItemDateImg.className = "list-group-item-date-img";
+  listGroupItemDateMoney.className = "list-group-item-date-money";
+  textDate.innerText = date;
+  textMoney.innerText = `${money} р.`;
+  listGroupItemImg.className = "list-group-item-img";
+  editImg.src = "https://img.icons8.com/ios/100/000000/edit--v1.png";
+  delImg.src = "https://img.icons8.com/ios/100/000000/waste.png";
+
+  editImg.onclick = () => editItem(index);
+  delImg.onclick = () => delItem(index);
+
+  listGroupItemImg.appendChild(editImg);
+  listGroupItemImg.appendChild(delImg);
+  listGroupItemDateMoney.appendChild(textDate);
+  listGroupItemDateMoney.appendChild(textMoney);
+  listGroupItemDateImg.appendChild(listGroupItemDateMoney);
+  listGroupItemDateImg.appendChild(listGroupItemImg);
+
+  const obj = {
+    name: nameShop,
+    date: listGroupItemDateImg,
+  };
+
+  return obj;
+};
+
+const closeChange = (index, shop, date, money) => {
+  const listGroupItem = document.getElementById(`item-${index}`);
+  listGroupItem.className = "list-group-item";
+
+  while (listGroupItem.firstChild) {
+    listGroupItem.removeChild(listGroupItem.firstChild);
+  }
+
+  const objItem = createItem(index, shop, date, money);
+
+  listGroupItem.appendChild(objItem.name);
+  listGroupItem.appendChild(objItem.date);
+};
+
+const changeItem = (index) => {
+  const inputName = document.getElementById(`text-${index}`);
+  const inputDate = document.getElementById(`date-${index}`);
+  const inputNewMoney = document.getElementById(`money-${index}`);
+
+  let newDate = "";
+  const subDate = inputDate.value.split("-");
+  newDate = subDate[2] + "." + subDate[1] + "." + subDate[0];
+
+  arrayShoping[index].shop = inputName.value;
+  arrayShoping[index].date = newDate;
+  arrayShoping[index].money = inputNewMoney.value;
+
+  localStorage.setItem("shop", JSON.stringify(arrayShoping));
+
+  updateResult();
+
+  const { shop, date, money } = arrayShoping[index];
+  closeChange(index, shop, date, money);
 };
